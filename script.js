cd7c516bd9fd799d7d9f24366fef863cf6872901
Abhishek Kumar Upadhayay <upadhayayabhishek15@gmail.com>
2024-03-25 10:22:06 +0530

    new version

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-let height=document.querySelector("#height");
-let weight=document.querySelector("#weight");
-let cal=document.querySelector("#calc");
 
-const calculateValue=(height,weight)=>{
-     
-    height=height/100;
-    return Math.floor(weight/(Math.pow(height,2)));
+let cal = document.querySelector("#calc");
 
-}
-const showResult=(bmi)=>{
-    let dis= document.querySelector(".result");
-    dis.innerHTML=`Bmi Score = ${bmi}`;
-    dis.style.padding="5px";
-    dis.style.fontSize="20px";
-    dis.style.backgroundColor="white";
-    dis.style.color="green";
-    
+
+
+const invalidMsg = () => {
+    let dis = document.querySelector(".result");
+    dis.innerHTML = "Inavlid Input";
+    dis.style.padding = "5px";
+    dis.style.fontSize = "20px";
+    dis.style.backgroundColor = "white";
+    dis.style.color = "green";
 }
 
-cal.addEventListener("click",()=>{
-   let Bmi = calculateValue(height.value ,weight.value);
-    showResult(Bmi);
-    
-})
+// const resetBtn=()=>{
+
+// }
+const calculateValue = (height, weight) => {
+    height = height / 100;
+    return Math.floor(weight / (Math.pow(height, 2)));
+
+}
+const showResult = (bmi) => {
+    let dis = document.querySelector(".result");
+    dis.innerHTML = `Bmi Score = ${bmi}`;
+    dis.style.padding = "5px";
+    dis.style.fontSize = "20px";
+    dis.style.backgroundColor = "white";
+    dis.style.color = "green";
+}
+
+cal.addEventListener("click", (e) => {
+    e.preventDefault();
+    let height = parseInt(document.querySelector("#height").value);
+    let weight = parseInt(document.querySelector("#weight").value);
+    console.log(height,weight);
+    if ((height === '' || height < 0 || isNaN(height)) && (weight=== '' || weight < 0 || isNaN(weight))) {
+        invalidMsg();
+    }
+    else {
+        let Bmi = calculateValue(parseInt(height), parseInt(weight));
+        showResult(Bmi);
+    }
+    //    resetBtn();
+});
+
